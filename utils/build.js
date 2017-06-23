244a79249dc6ca5abd89881caf893af4e6e15b9e
yannis <yannis.jonckheere@gmail.com>
2017-06-23 16:32:57 +0200

    Add Input File Component (image upload only)

--- utils/build.js
+++ utils/build.js
@@ -6,51 +6,55 @@
 var fs = require('fs');
 
 var args = process.argv.slice(2);
-var io   = args.indexOf('-o'),
-	is   = args.indexOf('-s');
+var io = args.indexOf('-o'),
+	is = args.indexOf('-s');
 
-function esc(msg){
+function esc(msg) {
 	console.log(msg);
 	process.exit(1);
 }
 
-if(args.length != 4){
+if (args.length != 4) {
 	esc('Wrong argl. Usage: build -s customStylePath -o outFileDir');
-} else if( io == -1){
+} else if (io == -1) {
 	esc('-o outFileDir argument missing.');
-} else if( is == -1){
+} else if (is == -1) {
 	esc('-s customStylePath argument missing');
 }
 
 var path = require('path'),
 	root = process.cwd();
 
-var through    = require('through'),
-	CleanCSS   = require('clean-css'),
+var through = require('through'),
+	CleanCSS = require('clean-css'),
 	browserify = require('browserify'),
-	UglifyJS   = require('uglify-js');
+	UglifyJS = require('uglify-js');
 
 process.chdir(__dirname);
 
-var outFileDir       = path.join(root,args[io + 1]),
-	customStylePath  = path.join(root,args[is + 1]),
+var outFileDir = path.join(root, args[io + 1]),
+	customStylePath = path.join(root, args[is + 1]),
 	defaultStylePath = path.resolve('../lib/core/document/Style.js');
 
-var style = through(function(){
-						this.queue('var Style = { string : "' +
-								   new CleanCSS().minify(fs.readFileSync(customStylePath).toString()) +
-						           '" }; module.exports = Style;');
-					});
+console.log(outFileDir, customStylePath)
 
-browserify('../index.js',{
-	standalone:'ControlKit',
-	debug:true})
-	.transform(function(file){
-		return file !== defaultStylePath ? through() : style;})
+var style = through(function () {
+	this.queue('var Style = { string : "' +
+		new CleanCSS().minify(fs.readFileSync(customStylePath).toString()) +
+		'" }; module.exports = Style;');
+});
+
+browserify('../index.js', {
+	standalone: 'ControlKit',
+	debug: true
+})
+	// .transform(function (file) {
+	// return file !== defaultStylePath ? through() : style;
+	// })
 	.bundle()
 	.pipe(fs.createWriteStream(
-		path.join(outFileDir,'controlKit.js')).on(
-		'close', function(){
-			fs.writeFileSync(path.join(outFileDir,'controlKit.min.js'),
-							 UglifyJS.minify(this.path).code);
+		path.join(outFileDir, 'controlKit.js')).on(
+		'close', function () {
+			fs.writeFileSync(path.join(outFileDir, 'controlKit.min.js'),
+				UglifyJS.minify(this.path).code);
 		}));
